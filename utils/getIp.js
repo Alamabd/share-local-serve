b21b75ac00c5a182a9150c51229308e7c78742da
Alamabd <alamabdillah0108@gmail.com>
2024-12-10 19:24:43 +0700

    feat: validate platform suport

--- utils/getIp.js
+++ utils/getIp.js
@@ -1,14 +1,18 @@
-const { networkInterfaces } = require('os')
+const { networkInterfaces, platform } = require('os')
 
 function getIp() {
-    return new Promise((resolve) => {
-        // // Ip address
-        const nets = networkInterfaces()
-        let ip = ''
-        if(nets.wlp3s0) {
-            resolve(nets.wlp3s0[0].address)
+    return new Promise((resolve, reject) => {
+        if(platform() === "linux") {
+            // // Ip address
+            const nets = networkInterfaces()
+            let ip = ""
+            if(nets.wlp3s0) {
+                resolve(nets.wlp3s0[0].address)
+            } else {
+                reject("Error(linux): No found ip address please check connection")
+            }
         } else {
-            resolve(null)
+            reject(`Error(platform): ${platform()} is not support or development`)
         }
     })
 }
